6bf30d7fba3455142a2e2a6ca6361c0ab9990e3d
AsherTornado2 <74437289+AsherTornado2@users.noreply.github.com>
2022-07-26 16:34:32 -0500

    Delete demo directory

--- demo/fullscreen.js
+++
@@ -1,9 +0,0 @@
-function goFullScreen(){
-    var canvas = document.getElementById("canvas1");
-    if(canvas.requestFullScreen)
-        canvas.requestFullScreen();
-    else if(canvas.webkitRequestFullScreen)
-        canvas.webkitRequestFullScreen();
-    else if(canvas.mozRequestFullScreen)
-        canvas.mozRequestFullScreen();
-}
